98649ab382ac3a361de220ad6fc4a532e6144e4a
mehmeteminsalim <mehmeteminsalim@gmail.com>
2024-02-03 14:08:29 +0300

    build:demo page & component folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import Autocomplete from "./components/ui/autocomplete";
 
-type Props = {}
-
-const App = (props: Props) => {
+const App = () => {
   return (
-    <div className='text-red-500'>App</div>
-  )
-}
+    <div className="w-full h-screen flex items-center justify-center">
+      <div className="flex flex-col gap-y-5 items-center">
+        <div>
+          <h1 className="text-5xl font-bold ">Autocomplete Demo</h1>
+        </div>
+        <Autocomplete />
+        <div className="flex gap-x-3 underline text-xs">
+          <a href="">Github</a>
+          <a href="">Linkedin</a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/ui/autocomplete/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Autocomplete = (props: Props) => {
+  return (
+    <div>Autocomplete</div>
+  )
+}
+
+export default Autocomplete
